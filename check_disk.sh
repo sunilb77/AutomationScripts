5d192c47bc1604d0bada32c3fed26e49f4c6a7c1
sunilbhamare77 <70826380+sunilbhamare77@users.noreply.github.com>
2021-12-23 09:06:03 -0500

    Update check_disk.sh

--- check_disk.sh
+++ check_disk.sh
@@ -1,15 +1,11 @@
-#!/bin/bash
-
-CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
-
-THRESHOLD=90
-
-if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
-
-mail -s 'Disk Space Alert' admin@example.com << EOF
-
-Your root partition remaining free space is critically low. Used: $CURRENT%
-
-EOF
-
-fi
+#!/bin/sh
+df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read output;
+do
+  echo $output
+  usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
+  partition=$(echo $output | awk '{ print $2 }' )
+  if [ $usep -ge 90 ]; then
+    echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" |
+     mail -s "Alert: Almost out of disk space $usep%" you@somewhere.com
+  fi
+done
