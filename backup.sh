9cad11f7e3f43631c3cc78f99d66264b554200d2
Sunil Bhamare <70826380+sunilb77@users.noreply.github.com>
2023-02-13 23:45:44 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
-backup_dirs=("/etc" "/home" "/boot")
-dest_dir="/backup"
-dest_server="server1"
-backup_date=$(date +%b-%d-%y)
+# Define the backup directory
+backup_dir="/backup"
 
-echo "Starting backup of: ${backup_dirs[@]}"
+# Define the date in a format that can be used in a file name
+date=$(date +%Y-%m-%d)
 
-for i in "${backup_dirs[@]}"; do
-sudo tar -Pczf /tmp/$i-$backup_date.tar.gz $i
-if [ $? -eq 0 ]; then
-echo "$i backup succeeded."
-else
-echo "$i backup failed."
+# Define the name of the backup file
+backup_file="$backup_dir/backup-$date.tar.gz"
+
+# Define the directory that you want to backup
+src_dir="/data"
+
+# Define the email address to send the notification to
+email_address="you@example.com"
+
+# Create the backup directory if it doesn't exist
+if [ ! -d "$backup_dir" ]; then
+  mkdir "$backup_dir"
 fi
-scp /tmp/$i-$backup_date.tar.gz $dest_server:$dest_dir
-if [ $? -eq 0 ]; then
-echo "$i transfer succeeded."
-else
-echo "$i transfer failed."
-fi
-done
 
-sudo rm /tmp/*.gzecho "Backup is done."
+# Create the backup
+tar -czf "$backup_file" "$src_dir"
+
+# Check if the backup was successful
+if [ $? -eq 0 ]; then
+  # Print a message indicating that the backup was successful
+  echo "Backup was successful. Backup file: $backup_file"
+
+  # Send an email notification
+  echo "The backup was successful. Backup file: $backup_file" | mail -s "Backup Complete" "$email_address"
+else
+  # Print a message indicating that the backup failed
+  echo "Backup failed. Please check the logs for more information."
+
+  # Send an email notification
+  echo "The backup failed. Please check the logs for more information." | mail -s "Backup Failed" "$email_address"
+fi
